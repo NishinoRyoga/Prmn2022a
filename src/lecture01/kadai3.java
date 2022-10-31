8d82991b168f602564dff1c765fa9917aa8881f5
b2201800c <b2201800@photon.chitose.ac.jp>
2022-10-31 18:17:15 +0900

    kada3完了

---
+++ src/lecture01/kadai3.java
@@ -0,0 +1,13 @@
+package lecture01;
+
+public class kadai3 {
+    public static void main(String[] args) {
+        int age = 21;
+        if(age<20) {
+            System.out.println("I am" + age + " years old so I cannot drink liquor.");
+        }
+        else{
+            System.out.println("I am " + age + " years old so I can drink liquor.");
+        }
+    }
+}
