1300ed237d70e4c1a1cdf7d5fbaff7668ba6cdec
b2201800c <b2201800@photon.chitose.ac.jp>
2022-11-06 17:22:20 +0900

    kada1完了

---
+++ src/lecture02/Excise2_1.java
@@ -0,0 +1,13 @@
+package lecture02;
+
+public class Excise2_1 {
+    public static void main(String[] args) {
+        Human human1 = new Human("たかし", 18);
+        Human human2 = new Human("ひろし", 20);
+        Human human3 = new Human("あつし", 23);
+
+        human1.print();
+        human2.print();
+        human3.print();
+    }
+}
