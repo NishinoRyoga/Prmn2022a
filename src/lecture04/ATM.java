020d6758a0053c95c0d3bb5c04277375a3fccc65
b2201800c <b2201800@photon.chitose.ac.jp>
2022-11-20 18:23:59 +0900

    kada2完成

--- src/lecture04/ATM.java
+++ src/lecture04/ATM.java
@@ -1,4 +1,59 @@
 package lecture04;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ATM {
+    List<Account> accountList;
+
+    public ATM(){
+        this.accountList = new ArrayList<>();
+    }
+
+    //アカウント登録
+    public void registerAccount(String name,String number){
+        Account account = new Account(name,number);
+        accountList.add(new Account(name,number));
+        System.out.println(name + " さんのアカウントを口座番号：" + number + "で登録しました。");
+    }
+
+    //アカウント有無の確認
+    public boolean existsAccount(String name,String number){
+        for (Account account : accountList){
+            if(account.getName() != null && account.getNumber() != null){
+                System.out.println("名前：" + name + " 口座番号：" + number + " は存在します。");
+                return true;
+            }
+        }
+        System.out.println("名前：" + name + " 口座番号：" + number + " は存在しません。");
+        return false;
+    }
+
+    //入金
+    public void deposit(String number,long money){
+        for(Account account : accountList){
+            if(account.getNumber().equals(number)){
+                account.setBalance(account.getBalance() + money);
+                System.out.println("口座番号：" + number + " に " + money + " 円入金しました。");
+            }
+        }
+    }
+
+    //出金
+    public long withdraw(String number,long money){
+        for(Account account : accountList){
+            if(account.getNumber().equals(number)) {
+                if (account.getBalance() >= money) {
+                    account.setBalance(account.getBalance() - money);
+                    System.out.print("口座番号：" + number + " から " + money + " 円引き出しました。");
+                    System.out.println("残高：" + account.getBalance() + "円です。");
+                    return account.getBalance();
+                }
+                System.out.print("口座番号：" + number + " から " + money + " 円引き出せませんでした。");
+                System.out.println("残高：" + account.getBalance() + "円です。");
+                return account.getBalance();
+            }
+        }
+        return 0;
+    }
 }
--- src/lecture04/Exercise4_2.java
+++ src/lecture04/Exercise4_2.java
@@ -1,4 +1,17 @@
 package lecture04;
 
 public class Exercise4_2 {
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+
+        String name = "藤井智子";
+        String number = "12345";
+
+        atm.existsAccount(name,number);
+        atm.registerAccount(name,number);
+        atm.existsAccount(name,number);
+        atm.deposit(number,1000);
+        atm.withdraw(number,2000);
+        atm.withdraw(number,500);
+    }
 }
